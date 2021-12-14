9661c8db0aa0cc9b2a5eac1705883ae8e4df5a4c
Gulsara1809 <92300038+Gulsara1809@users.noreply.github.com>
2021-12-14 16:17:13 +0600

    Add files via upload

---
+++ MyListsTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.MyListsPageObject;
+import lib.ui.NavigationUI;
+import lib.ui.SearchPageObject;
+import lib.ui.factories.ArticlePageObjectFactory;
+import lib.ui.factories.MyListsPageObjectFactory;
+import lib.ui.factories.NavigationUIFactory;
+import lib.ui.factories.SearchPageObjectFactory;
+import org.junit.Test;
+import lib.Platform;
+
+
+public class MyListsTest extends CoreTestCase {
+    private static final String name_of_folder = "Learning programming";
+    @Test
+    //ne
+
+    public void testSaveFirstArticleToMyList ()
+    {
+        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = ArticlePageObjectFactory.get(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+if(Platform.getInstance().isAndroid())
+        { ArticlePageObject.addArticleToMyList(name_of_folder);
+        }else {
+    ArticlePageObject.addArticlesToMySaved();
+}
+
+        ArticlePageObject.closeArticle();
+
+        NavigationUI NavigationUI = NavigationUIFactory.get(driver);
+        NavigationUI.clickMyLists();
+
+        MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
+        if (Platform.getInstance().isAndroid()) {
+            MyListsPageObject.openFolderByName(name_of_folder);
+        }
+        MyListsPageObject.swipeByArticleToDelete(article_title);
+
+    }
+}
